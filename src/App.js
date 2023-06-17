c58c6a1123cb6a1179b7b9ad9075bb3760a23410
siham <sihamheddi945@gmail.com>
2023-06-17 10:24:29 +0100

    usereducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 
 //import './App.css';
 
-import { useCounter } from "./useCounter";
+import { useReducer } from "react";
 
+
+
+const reducer = (state,action)=>{
+    switch (action.type) {
+      case "increment":
+         
+         return {counter:state.counter+1,isshow:state.isshow};
+      case "decrement":
+         return {counter:state.counter-1,isshow:state.isshow};
+      case "toggle":
+         return {counter:state.counter,isshow:!state.isshow}
+      default:
+         return {counter:state.counter,isshow:state.isshow}
+         
+    }
+}
 
 
 function App() {
-   const  [counter,increment,decrement,restart]= useCounter()
+   const [state, dispatch] = useReducer(reducer, {
+      counter:0,
+      isshow:false
+   })
    
   return (
     <div >
-       <p>counter : {counter}</p>
-       <button onClick={increment}>
-          increment
-       </button>
-       <button  onClick={decrement}>
-           decrement
-       </button>
-       <button  onClick={restart}>
-          restart
-       </button>
+         <h3>Counter :</h3>
+         <p>{state.counter}</p>
+           <button onClick={()=>dispatch({type:"increment"})}>
+              increment
+           </button>
+           <button onClick={()=>dispatch({type:"decrement"})}>
+               decrement
+           </button>
+          <button onClick={()=>dispatch({type:"toggle"})}>toggle the text</button>
+          {state.isshow?<p>lorem sdkkdsskkds dsksdqkkdskqsdk qskdkdserpe dqsjjdqsjds</p>:""}
     </div>
   );
 }
